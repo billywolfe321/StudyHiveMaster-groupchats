08dd39dc5ed7b64ecc3d2b3cd33033b8b535554c
billywolfe321 <billywolfe2003@gmail.com>
2024-04-15 11:37:40 +0100

    changes

--- lib/CustomAppBar.dart
+++ lib/CustomAppBar.dart
@@ -1,163 +1,38 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'OtherUserProfile.dart'; // Ensure this import is correct
+import 'SearchScreen.dart'; // Ensure this is imported if you're using SearchScreen for navigation
 
-class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
-  @override
-  _CustomAppBarState createState() => _CustomAppBarState();
-
-  @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
-}
-
-class _CustomAppBarState extends State<CustomAppBar> {
-  final TextEditingController _searchController = TextEditingController();
-  bool _isSearching = false;
-  List<dynamic> _searchResults = [];
-  Timer? _debounce;
-
-  @override
-  void initState() {
-    super.initState();
-    _searchController.addListener(_onSearchChanged);
-  }
-
-  @override
-  void dispose() {
-    _searchController.removeListener(_onSearchChanged);
-    _searchController.dispose();
-    _debounce?.cancel();
-    super.dispose();
-  }
-
-  _onSearchChanged() {
-    if (_debounce?.isActive ?? false) _debounce?.cancel();
-    _debounce = Timer(const Duration(milliseconds: 500), () {
-      if (_searchController.text.isNotEmpty) {
-        _performSearch(_searchController.text);
-      } else {
-        setState(() => _searchResults.clear());
-      }
-    });
-  }
-
-  _performSearch(String query) async {
-    DatabaseReference ref = FirebaseDatabase.instance.ref('Users');
-    DatabaseEvent event = await ref.orderByChild('username')
-        .startAt(query)
-        .endAt('$query\uf8ff')
-        .once();
-
-    if (event.snapshot.exists) {
-      List<dynamic> results = [];
-      event.snapshot.children.forEach((child) {
-        var val = Map<String, dynamic>.from(child.value as Map);
-        val['key'] = child.key; // Store Firebase key or user ID
-        results.add(val);
-      });
-
-      setState(() {
-        _searchResults = results;
-      });
-    } else {
-      setState(() {
-        _searchResults = ['No results'];
-      });
-    }
-  }
-
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        AppBar(
-          elevation: 4,
-          centerTitle: false,
-          automaticallyImplyLeading: false,
-          backgroundColor: Color(0xffad32fe),
-          title: !_isSearching
-              ? GestureDetector(
-            onTap: () => Navigator.of(context).pushReplacementNamed('/home'),
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.menu, color: Colors.white),
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
-                  },
-                ),
-                Image.asset('assets/logo.png', width: 24),
-                SizedBox(width: 8),
-                Text("Study Hive", style: TextStyle(fontWeight: FontWeight.w400,
-                    fontSize: 14,
-                    color: Colors.white)),
-              ],
-            ),
-          )
-              : TextField(
-            controller: _searchController,
-            decoration: InputDecoration(
-              hintText: "Search...",
-              hintStyle: TextStyle(color: Colors.white),
-              border: InputBorder.none,
-            ),
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: [
-            if (!_isSearching)
-              IconButton(
-                icon: Icon(Icons.search, color: Colors.white),
-                onPressed: () {
-                  setState(() {
-                    _isSearching = true;
-                  });
-                },
-              ),
-            if (_isSearching)
-              IconButton(
-                icon: Icon(Icons.close, color: Colors.white),
-                onPressed: () {
-                  setState(() {
-                    _isSearching = false;
-                    _searchController.clear();
-                    _searchResults.clear();
-                  });
-                },
-              ),
+    return AppBar(
+      elevation: 4,
+      centerTitle: false,
+      backgroundColor: Color(0xffad32fe),
+      title: GestureDetector(
+        onTap: () => Navigator.of(context).pushReplacementNamed('/home'), // Navigate to the home screen when the title is tapped
+        child: Row(
+          children: [
+            Image.asset('assets/logo.png', width: 24), // Ensure you have this asset or replace it with your logo
+            SizedBox(width: 8),
+            Text("Study Hive", style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14, color: Colors.white)),
           ],
         ),
-        if (_isSearching && _searchResults.isNotEmpty) _buildSearchResults(),
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.search, color: Colors.white),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SearchScreen()), // Navigate to the SearchScreen
+            );
+          },
+        ),
+        // Include any other action icons here if needed
       ],
     );
   }
 
-  Widget _buildSearchResults() {
-    return Container(
-      color: Colors.white,
-      constraints: BoxConstraints(maxHeight: 300),
-      // Adjust the height as necessary
-      child: ListView.separated(
-        shrinkWrap: true,
-        itemCount: _searchResults.length > 5 ? 5 : _searchResults.length,
-        // Limit to 5 results
-        separatorBuilder: (context, index) => Divider(height: 1),
-        itemBuilder: (context, index) {
-          var result = _searchResults[index];
-          return ListTile(
-            title: Text(result['username'] ?? 'No results'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => OtherUserProfile(userID: result['key']),
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight); // AppBar's preferred height
 }
--- lib/ReportPage.dart
+++ lib/ReportPage.dart
@@ -1,21 +1,86 @@
 import 'package:flutter/material.dart';
 
-class ReportPage extends StatelessWidget {
-  final String forumId;
-  final String commentId; // Add this line
+class ReportPage extends StatefulWidget {
+  final String forumId;  // Pass the forum ID to know which forum is being reported
+  final String? commentId;  // Optional: Pass the comment ID if a comment is being reported
 
-  ReportPage({required this.forumId, required this.commentId});
+  ReportPage({required this.forumId, this.commentId});
+
+  @override
+  _ReportPageState createState() => _ReportPageState();
+}
+
+class _ReportPageState extends State<ReportPage> {
+  final List<String> reportReasons = [
+    "Offensive Content",
+    "Spam",
+    "Misinformation",
+    "Harassment",
+    "Other"
+  ];  // Example report reasons
+
+  String? selectedReason;
+
   @override
   Widget build(BuildContext context) {
+    bool isCommentReport = widget.commentId != null;
+    String titleText = isCommentReport ? "Report Comment" : "Report Forum";
+    String promptText = isCommentReport ? "Select a reason for reporting this comment:" : "Select a reason for reporting this forum:";
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Report'),
+        title: Text(titleText),
+        backgroundColor: Color(0xffad32fe),
       ),
-      body: Center(
-        child: Text('Report content goes here'),
-        // Add your reporting form or functionality here
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(promptText),
+            DropdownButton<String>(
+              value: selectedReason,
+              isExpanded: true,
+              onChanged: (value) {
+                setState(() {
+                  selectedReason = value;
+                });
+              },
+              items: reportReasons.map<DropdownMenuItem<String>>((String reason) {
+                return DropdownMenuItem<String>(
+                  value: reason,
+                  child: Text(reason),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Functionality to handle submit, currently does nothing
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text("Report Submitted"),
+                        content: Text("Your report has been submitted for review."),
+                        actions: [
+                          TextButton(
+                              onPressed: () => Navigator.of(context).pop(),
+                              child: Text("OK")
+                          ),
+                        ],
+                      );
+                    }
+                );
+              },
+              child: Text("Submit Report"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xffae32ff),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
